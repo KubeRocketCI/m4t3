8b7ad6821a6c071c5dd02f9aab724ceb5c3c7887
Mykola Marusenko <mykola_marusenko@epam.com>
2024-06-06 13:38:10 +0300

    Add source code

---
+++ src/main/java/com/epam/edp/demo/controller/HelloEdpController.java
@@ -0,0 +1,19 @@
+package com.epam.edp.demo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Optional;
+
+/**
+ * Author: Pavlo_Yemelianov
+ */
+@RestController
+public class HelloEdpController {
+
+    @GetMapping(value = "/api/hello")
+    public String hello() {
+        String hostname = Optional.ofNullable(System.getenv("HOSTNAME")).orElse("unknown");
+        return "Response received from pod: " + hostname;
+    }
+}
